2457856e9fd7db969a7648e0652b122b1c1e6ff9
Diga Widyaprana <digawp@gmail.com>
2014-03-27 19:55:02 +0800

    first iteration, untested

--- MarkovModel.java
+++ MarkovModel.java
@@ -1,43 +1,131 @@
 package cs2020;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 public class MarkovModel {
 
-	final char NOCHARACTER = (char)(255);
+	final char NOCHARACTER = (char) (255);
 	private int m_order;
-	long seed;
-	
-	MarkovModel (String text, int order) {
+	BufferedReader reader;
+	HashMap<String, int[]> hashmap = new HashMap<String, int[]>();
+	Random randomizer = new Random();
+
+	MarkovModel(String text, int order) throws IOException {
 		m_order = order;
-		
+
+		reader = new BufferedReader(new FileReader(text));
+		fillMap();
+		reader.close();
+
 	}
-	
+
+	private void fillMap() throws IOException {
+		// TODO Auto-generated method stub
+		StringBuilder stringBuffer = new StringBuilder(3);
+
+		for (int i = 0; i < m_order; i++) {
+			if (!reader.ready()) {
+				throw new IOException(
+						"Text length is too short to construct a Markov Model of order"
+								+ m_order + ".");
+			}
+			stringBuffer.append(reader.read());
+		}
+		int next = reader.read();
+		int[] asciiArray = new int[128];
+		asciiArray[next]++;
+		
+		hashmap.put(stringBuffer.toString(), asciiArray);
+		
+		while (reader.ready()) {
+			
+			// Updating stringBuffer
+			stringBuffer.deleteCharAt(0);
+			stringBuffer.append(next);
+			
+			next = reader.read();
+			
+			String key = stringBuffer.toString();
+			
+			if (hashmap.containsKey(key)) {
+				asciiArray = hashmap.get(key);
+			} else {
+				asciiArray = new int[128];
+			}
+			
+			asciiArray[next]++;
+			hashmap.put(key, asciiArray);
+			
+		}
+
+	}
+
 	int order() {
 		return m_order;
 	}
-	
+
 	int getFrequency(String kgram) throws IllegalArgumentException {
 		if (kgram.length() != m_order) {
-			throw new IllegalArgumentException("Length of string kgram must be equal to order.");
+			throw new IllegalArgumentException(
+					"Length of string kgram must be equal to order.");
 		}
-		return 0;
+		
+		int[] asciiArray = hashmap.get(kgram);
+		
+		int output = 0;
+		
+		if (asciiArray == null) {
+			return 0;
+		}
+		
+		for (int i = 0; i < asciiArray.length; i++) {
+			output += asciiArray[i];
+		}
+		return output;
+		
 	}
-	
-	int getFrequenct(String kgram, char c) {
+
+	int getFrequency(String kgram, char c) {
 		if (kgram.length() != m_order) {
-			throw new IllegalArgumentException("Length of string kgram must be equal to order.");
+			throw new IllegalArgumentException(
+					"Length of string kgram must be equal to order.");
 		}
-		return 0;
+
+		int[] asciiArray = hashmap.get(kgram);
+		
+		if (asciiArray == null) {
+			return 0;
+		}
+		
+		return asciiArray[c];
+		
 	}
-	
+
 	char nextCharacter(String kgram) {
 		if (kgram.length() != m_order) {
-			throw new IllegalArgumentException("Length of string kgram must be equal to order.");
+			throw new IllegalArgumentException(
+					"Length of string kgram must be equal to order.");
 		}
-		return NOCHARACTER;
+		int[] asciiArray = hashmap.get(kgram);
+		if (asciiArray == null) {
+			return NOCHARACTER;
+		}
+		ArrayList<Character> candidates = new ArrayList<Character>();
+		for (int i = 0; i < asciiArray.length; i++) {
+			while (asciiArray[i] > 0) {
+				candidates.add((char) i);
+			}
+		}
+		
+		return candidates.get(randomizer.nextInt(candidates.size()));
+		
 	}
-	
+
 	void setRandomSeed(long s) {
-		seed = s;
+		randomizer.setSeed(s);
 	}
-	
+
 }
--- TextGenerator.java
+++ TextGenerator.java
@@ -1,5 +1,42 @@
 package cs2020;
 
+import java.io.*;
+
 public class TextGenerator {
 
+	final char NOCHARACTER = (char) (255);
+	BufferedReader reader;
+	MarkovModel model;
+	int limit;
+	StringBuilder stringBuffer;
+	String start;
+	
+	TextGenerator(int k, int n, String text) throws IOException {
+			model = new MarkovModel(text, k);
+			reader = new BufferedReader(new FileReader(text));
+			limit = n;
+			
+			for (int i = 0; i < k; i++) {
+				stringBuffer.append(reader.read());
+			}
+			start = stringBuffer.toString();
+	}
+	
+	String run() {
+		int current_length = model.order();
+		
+		while (current_length <= limit) {
+			char next = model.nextCharacter(stringBuffer.toString());
+			if (next == NOCHARACTER) {
+				next = model.nextCharacter(start);
+			}
+			stringBuffer.append(next);
+		}
+		return stringBuffer.toString();
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
 }
