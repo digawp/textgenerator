ae8501dfcec4a366e2a4bb6735f9356520b5b780
Diga Widyaprana <digawp@gmail.com>
2014-03-28 22:45:17 +0800

    Comment out all methods

--- MarkovModel.java
+++ MarkovModel.java
@@ -5,136 +5,253 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
+/**
+ * The class MarkovModel to generate the MarkovModel of a sequence of characters
+ * of order k (passed to the constructor as argument)
+ * 
+ * @author Diga W
+ * 
+ */
 public class MarkovModel {
 
+	// A placeholder for the method nextCharacter() to inform that there is no
+	// next character available for the specified sequence of characters
 	final char NOCHARACTER = (char) (255);
+	// The order of the Markov Model
 	private int m_order;
+	// The reader
 	private BufferedReader reader;
+	// The hash table to map a sequence of characters to its possible next
+	// characters
 	HashMap<String, int[]> hashmap = new HashMap<String, int[]>();
+	// The randomizer for nextCharacter() method to randomly select the next
+	// character
 	Random randomizer = new Random();
 
+	/**
+	 * Constructor
+	 * 
+	 * @param text
+	 *            text that the Markov Model is based on
+	 * @param order
+	 *            the order of the Markov Model, which is the number of
+	 *            characters the next character prediciton is based on
+	 * @throws IOException
+	 */
 	MarkovModel(String text, int order) throws IOException {
 		m_order = order;
-
 		reader = new BufferedReader(new FileReader(text));
+		// See the method definition just below
 		fillMap();
 		reader.close();
 
 	}
 
+	/**
+	 * Fills the hash map with the sequence of characters and its possible set
+	 * of next character
+	 * 
+	 * @throws IOException
+	 */
 	private void fillMap() throws IOException {
 
+		// Uses StringBuilder as the stringBuffer as it is said to be more
+		// efficient
 		StringBuilder stringBuffer = new StringBuilder(3);
 
+		// Fills the buffer with the first k characters of the text, where k is
+		// the order of the Markov Model
 		for (int i = 0; i < m_order; i++) {
+			// If the text supplied is shorter than the order (which is stupid),
+			// throws an error
 			if (!reader.ready()) {
 				throw new IOException(
 						"Text length is too short to construct a Markov Model of order"
 								+ m_order + ".");
 			}
-			stringBuffer.append((char)reader.read());
+
+			stringBuffer.append((char) reader.read());
+
 		}
+
+		// The next character following the sequence of k characters
 		int next = reader.read();
+
+		// Store the possible set of next character in an array of integer of
+		// size 128 (the number of ASCII characters)
 		int[] asciiArray = new int[128];
+
+		// Increment the element that corresponds to that character
 		asciiArray[next]++;
-		
+
+		// put the array into the hash map, mapped to the sequence of character
+		// stored as string
 		hashmap.put(stringBuffer.toString(), asciiArray);
-		
+
+		// For the rest of the text
 		while (reader.ready()) {
-			
-			// Updating stringBuffer
+
+			// Updating stringBuffer, using some sort of 'rolling hash' (or is
+			// it
+			// indeed rolling hash?)
 			stringBuffer.deleteCharAt(0);
-			stringBuffer.append((char)next);
-			
+			stringBuffer.append((char) next);
+
+			// The next character that follows the sequence of k characters
 			next = reader.read();
-			
+
+			// store the string form of the buffer to avoid rebuilding the
+			// string for the next few checks
 			String key = stringBuffer.toString();
-			
+
 			if (hashmap.containsKey(key)) {
+				// If the hash map already contain the key, retrieve the array
 				asciiArray = hashmap.get(key);
 			} else {
+				// Else, create a new one
 				asciiArray = new int[128];
 			}
+
+			// Error checking on my side only, because some of the text sample I
+			// used contains some characters that is outside the 128 ASCII
+			// character, for whatever reason
 			if (next > 127) {
 				continue;
 			}
+
+			// Increment the appropriate character in the array
 			asciiArray[next]++;
+			// Put into the hash map
 			hashmap.put(key, asciiArray);
-			
+
 		}
 
 	}
 
+	/**
+	 * Retrieves the order of the Markov Model and return it
+	 * 
+	 * @return
+	 */
 	int order() {
 		return m_order;
 	}
 
+	/**
+	 * Counts the frequency that the specified String appeared in the sample
+	 * text supplied and return it
+	 * 
+	 * @param kgram
+	 *            String to be counted
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
 	int getFrequency(String kgram) throws IllegalArgumentException {
+
+		// Error check
 		if (kgram.length() != m_order) {
 			throw new IllegalArgumentException(
 					"Length of string kgram must be equal to order.");
 		}
-		
+
+		// Retrieve the array of the specified character sequence in string form
 		int[] asciiArray = hashmap.get(kgram);
-		
+
+		// Initialize the output
 		int output = 0;
-		
+
+		// If the hashmap retrieves nothing, means the specified string does not
+		// appear in the text
 		if (asciiArray == null) {
 			return 0;
 		}
-		
+
+		// Count the number of times the string kgram appears
 		for (int i = 0; i < asciiArray.length; i++) {
 			output += asciiArray[i];
 		}
+
 		return output;
-		
+
 	}
 
+	/**
+	 * Counts the number of times the specified character follows the specified
+	 * character sequence given in string form kgram
+	 * 
+	 * @param kgram
+	 * @param c
+	 * @return
+	 */
 	int getFrequency(String kgram, char c) {
+
+		// Error check
 		if (kgram.length() != m_order) {
 			throw new IllegalArgumentException(
 					"Length of string kgram must be equal to order.");
 		}
 
+		// Retrieve the array of the specified character sequence in string form
 		int[] asciiArray = hashmap.get(kgram);
-		
+
+		// If the hashmap retrieves nothing, means the specified string does not
+		// appear in the text
 		if (asciiArray == null) {
 			return 0;
 		}
-		
+
 		return asciiArray[c];
-		
+
 	}
 
+	/**
+	 * Generates the next character from the given sequence of characters given
+	 * as a string kgram
+	 * 
+	 * @param kgram
+	 * @return
+	 */
 	char nextCharacter(String kgram) {
+
+		// Error check
 		if (kgram.length() != m_order) {
 			throw new IllegalArgumentException(
 					"Length of string kgram must be equal to order.");
 		}
-		
-		
-		// TODO doesn't work for some reason
+
+		// If the hashmap obtains null from the given string, means there is no
+		// possible next character for the given string
 		if (hashmap.get(kgram) == null) {
+			// Returns the placeholder for no character
 			return NOCHARACTER;
 		}
-		
+
+		// Creates a clone of the array as I am going to modify it for deciding
+		// the next character
 		int[] asciiArray = hashmap.get(kgram).clone();
-		
+
+		// Initialize a list of candidate characters
 		ArrayList<Character> candidates = new ArrayList<Character>();
+
+		// Add the characters into the list according to the number of times it
+		// appears after the given string in the text
 		for (int i = 0; i < asciiArray.length; i++) {
 			while (asciiArray[i] > 0) {
 				candidates.add((char) i);
 				asciiArray[i]--;
 			}
 		}
-		if (candidates.size() == 0) {
-			return NOCHARACTER;
-		}
+
+		// Generate a random number between 0 inclusive and the size of the candidates list exclusive
 		int get = randomizer.nextInt(candidates.size());
 		return candidates.get(get);
-		
+
 	}
 
+	/**
+	 * Sets a seed for the randomizer, for debugging purposes
+	 * @param s
+	 */
 	void setRandomSeed(long s) {
 		randomizer.setSeed(s);
 	}
--- TextGenerator.java
+++ TextGenerator.java
@@ -2,49 +2,100 @@ package cs2020;
 
 import java.io.*;
 
+/**
+ * A class that generates a string of n characters from the given text file to
+ * be used as the Markov Model
+ * 
+ * @author Diga W
+ * 
+ */
 public class TextGenerator {
 
-	// final char NOCHARACTER = (char) (255);
+	// The file reader
 	BufferedReader reader;
+	// The Markov Model
 	MarkovModel model;
+	// The number of characters to generate
 	int limit;
+	// The string buffer
 	StringBuilder stringBuffer = new StringBuilder(3);
+	// The starting string, to be used in case there is no next character
+	// available in the model
 	String start;
-	
+
+	/**
+	 * Constructor
+	 * 
+	 * @param k
+	 *            the order of the Markov Model
+	 * @param n
+	 *            the number of characters to generate
+	 * @param text
+	 *            the text file the Markov Model is based on
+	 * @throws IOException
+	 */
 	TextGenerator(int k, int n, String text) throws IOException {
-			model = new MarkovModel(text, k);
-			reader = new BufferedReader(new FileReader(text));
-			limit = n;
-			
-			for (int i = 0; i < k; i++) {
-				stringBuffer.append((char)reader.read());
-			}
-			reader.close();
-			start = stringBuffer.toString();
+		model = new MarkovModel(text, k);
+		reader = new BufferedReader(new FileReader(text));
+		limit = n;
+
+		// Initialize the string buffer
+		for (int i = 0; i < k; i++) {
+			stringBuffer.append((char) reader.read());
+		}
+		reader.close();
+		// Initialize the starting string.
+		start = stringBuffer.toString();
 	}
-	
+
+	/**
+	 * Runs the Text Generator and returns the output string
+	 * 
+	 * @return
+	 */
 	String run() {
-		int currentLength = model.order();
+
+		// Initialize the output
+		// Using the StringBuilder as it is said to be more efficient than keeps
+		// concantenating characters to a String, which will keep creating a new
+		// String each time it is concatenated
 		StringBuilder output = new StringBuilder(start);
+
+		// keeps track of the current length of the output
+		int currentLength = model.order();
+
+		// Repeat until the length of output is satisfied
 		while (currentLength <= limit) {
-			char next = model.nextCharacter(stringBuffer.toString());
 			
-			// TODO doesn't work
+			// The next character to append
+			char next = model.nextCharacter(stringBuffer.toString());
+
+			// Checks if it is a NOCHARACTER
 			if (next == model.NOCHARACTER) {
+				// If it is, starts from the beginning
+				output.append(start);
 				next = model.nextCharacter(start);
 			}
+			
+			// Appends the next character to the StringBuilder output
 			output.append(next);
+			
+			// Update the string buffer using 'rolling hash' method
 			stringBuffer.append(next);
 			stringBuffer.deleteCharAt(0);
+			
+			// increment current length
 			currentLength++;
 		}
+		
 		return output.toString();
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		TextGenerator textgen = new TextGenerator(5, 100, "test.txt");
-		textgen.model.setRandomSeed(100);
+		// test.txt contains texts taken from http://obamaspeeches.com/
+		TextGenerator textgen = new TextGenerator(5, 180, "test.txt");
+		textgen.model.setRandomSeed(1);
 		System.out.println(textgen.run());
 	}
-	
+
 }
